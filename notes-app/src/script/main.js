0ce8b6bc23780b0310a11fb8207802ea86c2aac9
DentaAzman <152490890+DentaAzman@users.noreply.github.com>
2024-03-30 12:57:06 +0700

    Update main.js

--- notes-app/src/script/main.js
+++ notes-app/src/script/main.js
@@ -2,21 +2,20 @@ import notesData from "./data/notes-data.js";
 import './components/index.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const formNote = document.getElementById('formNote');
   const notesLists = document.getElementById('notesLists');
 
-  formNote.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const title = formNote.elements.title.value;
-    const body = formNote.elements.body.value;
-
-    addNoteToLists(title, body);
-
-    formNote.reset();
-  });
-
   function addNoteToLists(title, body) {
+    const noteObject = {
+      id: `notes-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`,
+      title: title,
+      body: body,
+      createdAt: new Date().toISOString(),
+      archived: false,
+    };
+
+    notesData.push(noteObject);
+
+    
     const noteElement = document.createElement('div');
     noteElement.classList.add('notes');
     noteElement.innerHTML = `
@@ -26,52 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
     notesLists.appendChild(noteElement);
   }
 
+  const formInput = document.querySelector('form-input');
+
+  formInput.addEventListener('submit', function (event) {
+    addNoteToLists(event.detail.title, event.detail.body);
+  });
+
   notesData.forEach(function (notes) {
     addNoteToLists(notes.title, notes.body);
   });
 });
-
-
-const unfilledForm = document.querySelector('form');
-const titleInput = unfilledForm.elements.title;
-const bodyInput = unfilledForm.elements.body;
-
-unfilledForm.addEventListener('submit', (event) => event.preventDefault());
-
-const customValidationInputHandler = (event) => {
-  event.target.setCustomValidity('');
-
-  if (event.target.validity.valueMissing) {
-    event.target.setCustomValidity('Harus diisi.');
-    return;
-  }
-};
-
-titleInput.addEventListener('change', customValidationInputHandler);
-titleInput.addEventListener('invalid', customValidationInputHandler);
-
-bodyInput.addEventListener('input', customValidationInputHandler);
-bodyInput.addEventListener('invalid', customValidationInputHandler);
-
-titleInput.addEventListener('blur', (event) => {
-  updateValidationMessage(event.target);
-});
-
-bodyInput.addEventListener('blur', (event) => {
-  updateValidationMessage(event.target);
-});
-
-function updateValidationMessage(input) {
-  const isValid = input.validity.valid;
-  const errorMessage = input.validationMessage;
-  
-  const connectedValidationId = input.getAttribute('aria-describedby');
-  const connectedValidationEl = connectedValidationId ?
-    document.getElementById(connectedValidationId) : null;
-
-  if (connectedValidationEl && errorMessage && !isValid) {
-    connectedValidationEl.innerText = errorMessage;
-  } else {
-    connectedValidationEl.innerText = '';
-  }
-}
